67e05286e553455601a8fe70d376b6ea38ec5c2e
Manu <esioniac@gmail.com>
2018-10-09 17:12:54 +0200

    Unit Test OK

--- src/app/signin-container/signin-container.component.spec.ts
+++ src/app/signin-container/signin-container.component.spec.ts
@@ -2,6 +2,10 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SigninContainerComponent} from './signin-container.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialConfigModule} from '../material-config/material-config.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('SigninContainerComponent', () => {
   let component: SigninContainerComponent;
@@ -10,7 +14,7 @@ describe('SigninContainerComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SigninContainerComponent ],
-      imports: [],
+      imports: [RouterTestingModule, BrowserAnimationsModule, FormsModule, ReactiveFormsModule, MaterialConfigModule],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
