88b3b9691fe96ebfec9aa82de28f49de9f965b96
Antony <antony.correoso@laposte.net>
2018-10-10 17:42:42 +0200

    Fix tests

--- src/app/login-container/login-container.component.spec.ts
+++ src/app/login-container/login-container.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginContainerComponent } from './login-container.component';
+import { MaterialConfigModule } from '../material-config/material-config.module';
+import {RouterTestingModule} from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('LoginContainerComponent', () => {
   let component: LoginContainerComponent;
@@ -8,7 +13,20 @@ describe('LoginContainerComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginContainerComponent ]
+      imports:[
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialConfigModule,
+        RouterTestingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [ 
+        LoginContainerComponent,
+       ],
+       providers: [
+          CookieService,
+      ],
     })
     .compileComponents();
   }));
