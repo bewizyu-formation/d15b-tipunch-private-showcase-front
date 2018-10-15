8eb841500f25045d9311d6abca92b1f167872283
AntonyCo <antony.correoso@laposte.net>
2018-10-16 00:19:25 +0200

    refactor variables name - fix travis

--- src/app/home-container/home-container.component.ts
+++ src/app/home-container/home-container.component.ts
@@ -11,8 +11,8 @@ import {Artist} from '../model/Artist';
   styleUrls: ['./home-container.component.css']
 })
 export class HomeContainerComponent implements OnInit {
-  artists: Observable<Artist[]> = new Observable<Artist[]>();
-  artist: Observable<Artist> = new Observable<Artist>();
+  artists$: Observable<Artist[]> = new Observable<Artist[]>();
+  artist$: Observable<Artist> = new Observable<Artist>();
 
   constructor(private artistService: ArtistService, private headerService: HeaderService) {
   }
@@ -20,10 +20,10 @@ export class HomeContainerComponent implements OnInit {
   ngOnInit() {
     this.artistService.findAllByDeptId(44);
     this.artistService.findById(10);
-    this.artists = from(this.artistService.getArtists$);
-    this.artist = from(this.artistService.getArtist$);
-    console.log(this.artists);
-    console.log(this.artist);
+    this.artists$ = from(this.artistService.getArtists$);
+    this.artist$ = from(this.artistService.getArtist$);
+    console.log(this.artists$);
+    console.log(this.artist$);
 
 
     this.headerService.emitChange('Artistes dans votre département', 'home');
